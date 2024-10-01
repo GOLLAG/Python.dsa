74f718d03fd584cab845e7793b7e91143fe872c7
GOLLAG <98303174+GOLLAG@users.noreply.github.com>
2024-10-01 14:58:20 +0530

    Create stack,queue.py

---
+++ stack,queue.py
@@ -0,0 +1,36 @@
+class Stack:
+  def _init_(self):
+    self.items=[]
+  def push(self,item):
+    self.items.append(item)
+  def pop(self):
+    if not self.is_empty():
+      return self.items.pop()
+    else:
+      return None
+  def peek(self):
+    if not self.is_empty():
+      return self.items[-1]
+  def is_empty(self):
+    return len(self.items) ==0
+  def _len_(self):
+    return len(self.items)
+  def _str_(self):
+    return str(self.items)
+
+
+stack=Stack()
+stack.push(10)
+stack.push(20)
+stack.push(40)
+stack.push(50)
+stack.push(70)
+stack.push(50)
+stack.push(270)
+stack.push(202)
+print(stack)
+p=stack.pop()
+print(p)
+q=stack.peek()
+print(q)
+print(stack)
